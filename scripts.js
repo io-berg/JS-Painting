e33d4a8ef997586473b463c1965fca8097e23a08
Daniel Berg <Berg.iodaniel@gmail.com>
2021-12-20 10:56:54 +0100

    rainbow mode implemented

--- scripts.js
+++ scripts.js
@@ -1,13 +1,30 @@
 const canvasElem = document.querySelector("#canvas");
 const colorPicker = document.querySelector("#colorInput");
 const resetBtn = document.querySelector("#resetBtn");
-let currentColor;
+const rainbowBtn = document.querySelector("#rainbowBtn");
 
-
+let colorMode = 1; // 1 for colorpicker, 2 for rainbow mode.
+let currentColor = "black" 
+rainbowBtn.addEventListener('click', () => colorMode == 1 ? colorMode = 2 : colorMode = 1);
 colorPicker.addEventListener("input", changeColor);
 
 function changeColor() {
+    if (colorMode == 2)
+        colorMode = 1;
+    
     currentColor = colorPicker.value;
+}
+
+function randomColor() {
+    let red = Math.floor(Math.random() * 257);
+    let green = Math.floor(Math.random() * 257);
+    let blue = Math.floor(Math.random() * 257);
+
+    return rgbToHex(red, green, blue);
+}
+
+function rgbToHex(r, g, b) {
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 }
 
 resetBtn.addEventListener("click", clearCanvas);
@@ -28,5 +45,10 @@ for (let i = 0; i < 16; i++){
 }
 
 function paintPixel(e) {
+    if (colorMode === 2) {
+        let color = randomColor();
+        colorPicker.value = color;
+        currentColor = color;
+    }
     e.target.style.backgroundColor = currentColor;
 }
